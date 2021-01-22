279cc691b195094c7e4b9677eb9cd2c86b82f30c
apizdose <apizdose@gmail.com>
2021-01-22 17:01:16 +0200

    First push

---
+++ TGinsta_photobot.py
@@ -0,0 +1,24 @@
+import telebot
+from telebot.types import Message
+import random
+import re
+import requests
+
+def tokenget():
+    with open('token.txt','r') as tokenfile:
+        return tokenfile.read()
+
+TOKENTG = tokenget()
+bot = telebot.TeleBot(TOKENTG)
+
+
+@bot.message_handler(content_types=['text'])
+@bot.edited_message_handler(content_types=['text'])
+def send_echo(message):
+    try:
+        urljunk =  re.findall(r"\?.*",message.text)[0]
+        url = message.text.replace(urljunk,"media?size=l")
+        bot.send_message(message.chat.id, url)
+    except:bot.send_message(message.chat.id, "Something went wrong (")	        
+
+bot.polling( none_stop = True)
